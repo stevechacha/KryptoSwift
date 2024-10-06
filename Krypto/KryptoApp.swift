9bac28bdefedafe245a4f368a15616fbae269163
Stephen  Chacha <48447675+stevechacha@users.noreply.github.com>
2024-10-06 21:45:50 +0300

    Initial Commit

---
+++ Krypto/KryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  KryptoApp.swift
+//  Krypto
+//
+//  Created by stephen chacha on 06/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct KryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KryptoTests/KryptoTests.swift
@@ -0,0 +1,17 @@
+//
+//  KryptoTests.swift
+//  KryptoTests
+//
+//  Created by stephen chacha on 06/10/2024.
+//
+
+import Testing
+@testable import Krypto
+
+struct KryptoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
